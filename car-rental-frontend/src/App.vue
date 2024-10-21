715bf80ef240813ac48e2fa8c0a606ad5dcebf31
Nico Mayr <nico_mayr@icloud.com>
2024-10-21 15:20:34 +0200

    visible icons

--- car-rental-frontend/src/App.vue
+++ car-rental-frontend/src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <v-app>
     <v-navigation-drawer app>
-      <v-list>
-        <v-list-item link to=\"/\">
-          <v-list-item-title>Dashboard</v-list-item-title>
+      <v-list dense>
+        <v-list-item :to="{ name: 'Dashboard' }" link>
+          <v-list-item-icon>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-        <v-list-item link to=\"/customers\">
-          <v-list-item-title>Customers</v-list-item-title>
+
+        <v-list-item :to="{ name: 'Customers' }" link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-group</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Customers</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-        <v-list-item link to=\"/cars\">
-          <v-list-item-title>Cars</v-list-item-title>
+
+        <v-list-item :to="{ name: 'Cars' }" link>
+          <v-list-item-icon>
+            <v-icon>mdi-car</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Cars</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
-        <v-list-item link to=\"/rentals\">
-          <v-list-item-title>Rentals</v-list-item-title>
+
+        <v-list-item :to="{ name: 'Rentals' }" link>
+          <v-list-item-icon>
+            <v-icon>mdi-car-key</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Rentals</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -30,5 +53,9 @@
 <script>
 export default {
   name: 'App',
-}
+};
 </script>
+
+<style>
+/* Add any global styles here */
+</style>
--- car-rental-frontend/src/main.js
+++ car-rental-frontend/src/main.js
@@ -1,14 +1,23 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
+// Import Material Design Icons CSS
+import '@mdi/font/css/materialdesignicons.css';
+
 Vue.config.productionTip = false;
+
 Vue.use(Vuetify);
 
 new Vue({
   router,
-  vuetify: new Vuetify(),
-  render: h => h(App)
+  vuetify: new Vuetify({
+    icons: {
+      iconfont: 'mdi', // Default - use 'mdi' for Material Design Icons
+    },
+  }),
+  render: h => h(App),
 }).$mount('#app');
